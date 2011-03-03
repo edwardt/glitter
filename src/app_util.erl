7599b03ce86bc1516092369151a71bf504a8b32b
developer <developer@developer-laptop.(none)>
2011-03-02 16:17:05 -0800

    Add some unit tests

--- src/app_util.erl
+++ src/app_util.erl
@@ -1,4 +1,5 @@
 -module (app_util).
+
 -author (edwardt.tril@gmail.com).
 
 -export([ensure_app_start/1, 
@@ -7,6 +8,11 @@
          get_value/2,
          get_value/3,
          set_value/3]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 
 ensure_app_start(AppName) when is_atom(AppName) ->
   case application:start(AppName) of 
@@ -43,3 +49,59 @@ set_value(App, Key, Value) when is_atom(App), is_atom(Key) ->
       {ok , _Val} -> ok;
       {error, Error} -> {error, Error}
   end.
+
+%% =========== unit tests ===========
+-ifdef(TEST).
+
+app_util_test_()->
+  { setup,
+    fun setup/0,
+    fun cleanup/1,
+    [
+      fun app_start_test_case/0,
+      fun app_already_start_test_case/0,
+      fun app_start_undef_app_test_case/0,
+      fun app_stop_test_case/0,
+      fun app_already_stop_test_case/0,
+      fun app_stop_undef_app_test_case/0,
+      fun set_appvalue_test_case/0,
+      fun set_appvalue_undef_key_test_case/0,
+      fun set_appvalue_undef_app_test_case/0
+      fun get_all_value_test_case/0,
+      fun get_value_test_case/0,
+      fun get_value_undef_app_test_case/0,
+      fun get_value_undef_key_test_case/0,
+      fun get_default_value_test_case/0
+    ]
+  }.
+
+app_start_test_case() ->
+  
+ .
+app_already_start_test_case()->
+ .
+app_start_undef_app_test_case()->
+ .
+app_stop_test_case()->
+ .
+app_already_stop_test_case()->
+ .
+app_stop_undef_app_test_case()->
+ .
+set_appvalue_test_case()->
+ .
+set_appvalue_undef_key_test_case()->
+ .
+set_appvalue_undef_app_test_case()->
+ .
+get_all_value_test_case()->
+ .
+get_value_test_case()->
+ .
+get_value_undef_app_test_case()->
+ .
+get_value_undef_key_test_case()->
+ .
+get_default_value_test_case()->
+ .
+-endif.
