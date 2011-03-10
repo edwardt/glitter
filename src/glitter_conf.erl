4c4b9df018e8dd3cf161fc15a573b3d433235fc4
edwardt <edwardt.tril@gmail.com>
2011-03-10 15:40:20 -0800

    add config

--- src/glitter_conf.erl
+++ src/glitter_conf.erl
@@ -1,24 +1,88 @@
 -module (glitter_conf).
 -author (edwardt.tril@gmail.com).
 
--export([get_value/1, 
-         get_value/2,
-         get_all_value/0,
-         update_value/2]).
+-include(log.hrl).
+
+-export([get_port/0, 
+         get_key_dir/0,
+         get_config_dir/0,
+	 get_log_name/0,
+         get_log_dir/0]).
 
 -behaviour(gen_server).
 %% API
--export([start_link/0]).
 
+-export([start_link/0]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
 -define (currentApp, glitter).
 
+-record(state, conf :: [atom(), term()]).
+
+start_link()->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
+config_spec()->
+  [ 
+    app_util::required(port_number),
+    app_util:required(key_dir),
+    app_util:required(config_dir),
+    app_util:optional(log_dir, "../log")
+  ].
 
+call(Key) when is_atom(Key)->
+  case gen_server:call(?MODULE, {get_param, Key}) of
+       undefined -> 
+		 exit(no_such_application_config_value);
+       Value -> Value
+  end.
+
+port_number()-> 
+  call(port_number).   
+
+key_dir()->
+  call(key_dir).
+
+config_dir()->
+  call(config_dir).
+
+log_name()->
+  call(log_name).
+
+log_dir()->
+  call(log_dir).
+
+
+init([])->
+  Configs = app_util:read_config([], config_spec()),
+  {ok, #state{config = Configs} }.  
+
+handle_call({get_param, Key}. From, State)->
+  ReplyValue = propslist:get_value(Key, #State.config),
+  {reply, ReplyValue, State}.
+
+handle_call(_InvalidMsg, _From, State)->
+   {ok, State}.
+
+handle_cast(_UnSupportedMsg, State)->
+  {no_reply, State}.
+
+handle_info(_A, State)->
+  {no_reply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+  
+
+terminate()->
+
+code_change()->
 
 
 get_all_value()->
@@ -34,30 +98,5 @@ update_value(Key, NewValue) ->
   app_util:set_value(?currentApp, Key, NewValue).
 
 
-required(Key) when is_atom(Key) ->
-  fun(ConfigKV)->
-	%get from in memory or else from config
-	Val = case proplist:get_value(Key, ConfigKV) of
-		 undefined -> 
-			case get_value(Key) of
-                             {ok, Value0} -> Value0;                       
-			     undefined -> undefined
- 	                end;
-		 Value -> Value
-              end, 
-        case Val of
-             undefined -> undefined;
-             AllVal -> {Key, AllVal}
-        end           
-  end.
 
-optional(Key, Default) when is_atom(Key) ->
-  fun(ConfigKV)->
-	%get from in memory or else from config
-	Val = case proplist:get_value(Key, ConfigKV, Default) of
-		 {ok, Value} -> Value; 
-	         _Else -> Default
-              end, 
-        {Key, Val}
-  end.
  
